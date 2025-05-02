1aa589abf6d487ca76b41455bf66e06fca9eddc0
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-05-02 17:20:51 +0530

    authrntication chnaged

--- src/routes/examSessionRouter.ts
+++ src/routes/examSessionRouter.ts
@@ -1,15 +1,15 @@
 import express from 'express'
-import { authorization } from '../middleware/authorization'
+import {authMiddleware, authorization} from '../middleware/authorization'
 import {loginAttempt} from '../controllers/user'
 import {addSession, deleteSession, getAllSession, updateSession} from "../controllers/examSession";
 
 const router = express.Router()
 
 
-router.post('/addSession', authorization, addSession)
-router.get('/getAllSession',authorization, getAllSession)
-router.put('/updateSession',authorization, updateSession)
-router.delete('/deleteSession/:sessionId', authorization, deleteSession);
+router.post('/addSession', authMiddleware, addSession)
+router.get('/getAllSession',authMiddleware, getAllSession)
+router.put('/updateSession',authMiddleware, updateSession)
+router.delete('/deleteSession/:sessionId', authMiddleware, deleteSession);
 
 
 export default router
